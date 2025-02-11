c08ac0634f42e6f298b1706749c02f285272ca7a
2rebcat <2rebcat@gmail.com>
2025-02-11 15:48:39 +0900

    add repo list

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Routes, Route } from 'react-router';
 import './App.css';
 import Footer from './Component/Footer';
 import Header from './Component/Header';
+import Container from './Component/Container';
 import Resume from './Component/Resume';
 import RepoList from './Component/RepoList';
 
@@ -12,6 +13,7 @@ function App() {
       <main>
         <Routes>
           <Route path="/" element={<RepoList/>}/>
+          <Route path=':course' element={<Container/>}/>
           <Route path="/AboutMe" element={<Resume/>}/>
         </Routes>
       </main>
