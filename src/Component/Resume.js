508bde0d8b03749a7e1a8c5fe32a324b6a06a0c4
2rebcat <2rebcat@gmail.com>
2025-02-26 18:40:50 +0900

    이력서 업로드

--- src/Component/Resume.js
+++ src/Component/Resume.js
@@ -5,34 +5,55 @@ function Resume(){
         <div class="container">
             <div class="content">
                 <div class="card">
-                    <h1 class="title">포트폴리오</h1>
-                    <h3 class="intro">두 눈이 빨개질 때까지 밤낮으로 새로운 것을 찾는 2rebcat 한성필입니다!</h3>
+                    <h1 class="title">Introduction</h1>
+                    <h3 class="intro">두 눈이 빨개질 때까지 밤낮으로 새로움을 찾아 배우는 2RebCat 한성필입니다!</h3>
                 </div>
 
                 <div class="card">
-                    <h2>학위</h2>
+                    <h2>EDUCATION</h2>
                     <ul class="info">
-                        <li><strong>학교:</strong> [대학교 이름]</li>
-                        <li><strong>전공:</strong> [전공명]</li>
-                        <li><strong>졸업 연도:</strong> [졸업 연도]</li>
+                        <li><strong>대학교:</strong> 상명대학교 학사</li>
+                        <li><strong>전공:</strong> 컴퓨터과학과 정보보안전공</li>
+                        <li><strong>졸업 연도:</strong> 2019년 2월</li>
                     </ul>
                 </div>
 
                 <div class="card">
-                    <h2>경력</h2>
+                    <h2>CERTIFICATES</h2>
                     <ul class="info">
-                        <li><strong>회사:</strong> [회사명]</li>
-                        <li><strong>직책:</strong> [직책]</li>
-                        <li><strong>근무 기간:</strong> [기간]</li>
-                        <li><strong>주요 업무:</strong> [주요 업무 내용]</li>
+                        <li><strong>정보보안기사</strong> 한국인터넷진흥원</li>
+                        <li><strong>정보처리기사</strong> 한국산업인력공단</li>
+                        <li><strong>개인정보관리사(CPPG)</strong> 한국CPO포럼</li>
                     </ul>
                 </div>
 
                 <div class="card">
-                    <h2>연락처</h2>
+                    <h2>BRIEF WORK HISTORTY</h2>
                     <ul class="info">
-                        <li><strong>이메일:</strong> [이메일 주소]</li>
-                        <li><strong>LinkedIn:</strong> <a href="[링크]" target="_blank">[링크]</a></li>
+                        <li><strong>회사:</strong> SK 쉴더스</li>
+                        <li><strong>부서:</strong> EQST Lab팀</li>
+                        <li><strong>직급:</strong> 책임</li>
+                        <li><strong>근무 기간:</strong> 2021년 ~ 현재</li>
+                        <li><strong>주요 업무:</strong> 정보보안 교육 시스템 개발 및 최신 해킹 기술 트랜드 연구</li>
+                    </ul>
+                    <br></br>
+                    <ul class="info">
+                        <li><strong>회사:</strong> The Korea Times LA 본사</li>
+                        <li><strong>부서:</strong> New Media Dept.</li>
+                        <li><strong>직급:</strong> Assistant</li>
+                        <li><strong>근무 기간:</strong> 2019년 ~ 2020년</li>
+                        <li><strong>주요 업무:</strong> 전자 신문 서비스 유지보수 및 서버 관리</li>
+                    </ul>
+                </div>
+
+                <div class="card">
+                    <h2>CONTACT</h2>
+                    <ul class="info">
+                        <li><strong>이메일:</strong> 2rebcat@gmail.com</li>
+                        {
+                            // 나중에 SNS 추가하기
+                            // <li><strong>LinkedIn:</strong> <a href="[링크]" target="_blank">[링크]</a></li>
+                        }
                     </ul>
                 </div>
             </div>
