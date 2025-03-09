9f299922fd233e358f57f475c5c8df4e3a6d3d3a
sugpilhan <sungpil.han@sk.com>
2025-03-09 18:07:38 +0900

    네비게이션 바 UI 개선

--- src/Component/LocalNav.js
+++ src/Component/LocalNav.js
@@ -1,16 +1,36 @@
 import { Link } from "react-router";
+import { useRef, useState, useEffect } from "react";
 
 import './LocalNav.css';
 
 function LocalNav({coursename, NavData}){
     const NavList = [...NavData];
+
+    const [isOpen, setIsOpen] = useState(true);
+    useEffect(() => {
+        const handleResize = () => {
+            setIsOpen(window.innerWidth >= 601);
+        };
+
+        window.addEventListener("resize", handleResize);
+        
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    const detailsRef = useRef(null);
+    const handleClickInside = () =>{
+        if (window.innerWidth < 601){
+            setIsOpen(false);
+        }
+    }
+
     return(
         <div className="nav-container">
-            <details className="nav-dropdown" open>
+            <details className="nav-dropdown" ref={detailsRef} open={isOpen} onToggle={() => setIsOpen(detailsRef.current?.open)}>
                 <summary className="nav-header">Contents</summary>
                 <div className="nav-list">
                     {NavList.map((item, i) => (
-                        <Link key={i} to={"/?menu="+coursename+"&title="+(i+1)} className="nav-atag">
+                        <Link key={i} to={"/?menu="+coursename+"&title="+(i+1)} className="nav-atag" onClick={handleClickInside}>
                             <p key={i} className="nav-item">{item}</p>
                         </Link>
                     ))}
